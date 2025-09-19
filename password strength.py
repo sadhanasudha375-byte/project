641200b03abd1a6eca9c57e0d843bdeb557616cb
sadhanasudha375-byte <sadhanasudha375@gmail.com>
2025-09-19 09:10:25 +0530

    Update password strength.py

--- password strength.py
+++ password strength.py
@@ -1,2 +1,19 @@
-Python 3.13.7 (tags/v3.13.7:bcee1c3, Aug 14 2025, 14:15:11) [MSC v.1944 64 bit (AMD64)] on win32
-Enter "help" below or click "Help" above for more information.
+import re
+
+def check_password(password):
+  
+    if len(password) < 8:
+        return "Weak Password  (Too short, must be at least 8 characters)"
+    if not re.search(r"[A-Z]", password):
+        return "Weak Password  (Add at least one uppercase letter)"
+    if not re.search(r"[a-z]", password):
+        return "Weak Password  (Add at least one lowercase letter)"
+    if not re.search(r"[0-9]", password):
+        return "Weak Password  (Add at least one number)"
+    if not re.search(r"[@#$%!&*?]", password):
+        return "Weak Password  (Add at least one special character)"
+
+    return "Strong Password "
+
+password = input("Enter a password: ")
+print(check_password(password))
